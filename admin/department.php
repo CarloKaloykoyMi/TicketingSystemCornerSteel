a963f921d2ecfe74f65651a1920439b6be6b3f05
evagraceest <evalyngrace.estrera@my.jru.edu>
2024-02-20 09:34:17 +0800

    updated

--- admin/department.php
+++ admin/department.php
@@ -6,6 +6,7 @@ include 'sidebar_navbar.php'
 <html lang="en">
 
 <head>
+<title>Department</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- admin/user.php
+++ admin/user.php
@@ -7,6 +7,7 @@ include 'sidebar_navbar.php'
 <html lang="en">
 
 <head>
+<title>Users</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
