c0843795568c31d0945c3d509d2b741bbf7554f8
Kaloy <johncarlo.astoveza@my.jru.edu>
2024-03-06 21:02:50 +0700

    Logout-Admin Audit Done

--- admin/logoutadmin.php
+++ admin/logoutadmin.php
@@ -1,7 +1,7 @@
 <?php
 include ('mysql_connect.php');
 session_start();
-$user_id =$_SESSION['user_id'];
+$user_id = $_SESSION['auth_user']['user_id'];
 session_destroy();
 unset($_SESSION['auth_user']['username']);
 unset($_SESSION['userid']);
